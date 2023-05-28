057a562e03bf0a71eea02007a6d66265b8cbf4b5
Mayron Santos <mayronsantos278@gmail.com>
2023-05-28 17:05:39 -0300

    adicionando demais metodos do crud

--- src/main/java/com/seeds/neuroapp/controller/UsuarioController.java
+++ src/main/java/com/seeds/neuroapp/controller/UsuarioController.java
@@ -2,10 +2,12 @@ package com.seeds.neuroapp.controller;
 
 import com.seeds.neuroapp.model.Usuario;
 import com.seeds.neuroapp.service.UsuarioService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -18,7 +20,28 @@ public class UsuarioController {
     }
 
     @PostMapping("/usuarios")
-    public Usuario salvarUsuario(@RequestBody Usuario usuario) throws Exception {
+    public Usuario salvarUsuario(@Valid @RequestBody Usuario usuario) throws Exception {
         return usuarioService.salvarUsuario(usuario);
+    }
+
+    @GetMapping("/usuarios/{id}")
+    public Usuario consultarPorId(@PathVariable Long id){
+        return usuarioService.consultarPorId(id);
+    }
+
+    @GetMapping("/usuarios")
+    public List<Usuario> listarTodosUsuarios(){
+        return usuarioService.consultarTodosUsuarios();
+    }
+
+    @PutMapping("/usuarios/{id}")
+    public Usuario atualizarUsuario(@Valid @RequestBody Usuario usuario, @PathVariable Long id){
+        return usuarioService.atualizarUsuario(usuario, id);
+    }
+
+    @DeleteMapping("/usuarios/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarUsuario(@PathVariable Long id){
+        usuarioService.deletarUsuario(id);
     }
 }
--- src/main/java/com/seeds/neuroapp/service/UsuarioService.java
+++ src/main/java/com/seeds/neuroapp/service/UsuarioService.java
@@ -1,9 +1,13 @@
 package com.seeds.neuroapp.service;
 
+import com.seeds.neuroapp.exception.BadRequestException;
+import com.seeds.neuroapp.exception.ResourceNotFoundException;
 import com.seeds.neuroapp.model.Usuario;
 import com.seeds.neuroapp.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UsuarioService {
@@ -17,9 +21,30 @@ public class UsuarioService {
 
     public Usuario salvarUsuario(Usuario usuario) throws Exception {
         if (usuarioRepository.findByEmail(usuario.getEmail())!= null){
-            throw new Exception("usaldkjf");// Mudar para exception personalizada http status code 400
+            throw new BadRequestException("Usuário já existente");
         }
-
         return usuarioRepository.save(usuario);
+    }
+
+    public Usuario consultarPorId(Long id){
+        return usuarioRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Usuário não encontrado"));
+    }
+
+    public List<Usuario> consultarTodosUsuarios(){
+        return usuarioRepository.findAll();
+    }
+
+    public Usuario atualizarUsuario(Usuario usuario, Long id){
+        if (!usuarioRepository.existsById(id)){
+           throw new ResourceNotFoundException("Usuário não encontrado");
+        }
+        return usuarioRepository.save(usuario);
+    }
+
+    public void deletarUsuario(Long id){
+        if(!usuarioRepository.existsById(id)){
+            throw new ResourceNotFoundException("Usuário não encontrado");
+        }
+        usuarioRepository.deleteById(id);
     }
 }
